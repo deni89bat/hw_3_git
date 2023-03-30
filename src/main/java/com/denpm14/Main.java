df63d58de7f3fc58bbd10c2f4bde4da053b16336
deni89bat <118809586+deni89bat@users.noreply.github.com>
2023-03-31 00:56:27 +0300

    Update Main.java

--- src/main/java/com/denpm14/Main.java
+++ src/main/java/com/denpm14/Main.java
@@ -1,7 +1,7 @@
 package com.denpm14;
 
 public class Main {
-    private static String msg ="Hello girls!!!!!!!!!!";
+    private static String msg ="Hello girls!";
     
     public static void main(String[] args) {
         System.out.println(msg);
