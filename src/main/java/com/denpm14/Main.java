c380a83ea2ebe1d7ab0b5271e714c77c9a072610
deni89bat <den.pm14@gmail.com>
2023-03-31 00:42:12 +0300

    conflict

--- src/main/java/com/denpm14/Main.java
+++ src/main/java/com/denpm14/Main.java
@@ -2,6 +2,11 @@ package com.denpm14;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello girls!");
+        System.out.println(getMessage());
     }
+
+    public static String getMessage() {
+        return "hello man";
+    }
+
 }
